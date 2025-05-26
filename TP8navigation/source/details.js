04bef7ab388be9fe7ab85e50472a5618bfe9168c
MICHELLE SOFIA GARBER <48521612@ort>
2025-05-26 12:13:28 -0300

    corregimos

--- TP8navigation/source/details.js
+++ TP8navigation/source/details.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text,  Button } from 'react-native';
 
 export default function Details() {
   return (
     <View>
       <Text >Detalles</Text>
+      <Button>ver detalles</Button>
     </View>
   ); 
   }
